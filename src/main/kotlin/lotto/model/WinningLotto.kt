009ab571146757989c6a80d8f76d2428bd5ad5cd
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-13 16:59:51 +0900

    feat(WinningLotto): 당첨 번호 오류 메시지 추가

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -4,11 +4,12 @@ private const val HIT_SIX = 6
 private const val HIT_FIVE = 5
 private const val HIT_FOUR = 4
 private const val HIT_THREE = 3
+private const val PREFIX = "[ERROR]"
 
 class WinningLotto(private val winningNumbers: Lotto, private val bonusNumber: Int) {
     init {
-        require(bonusNumber in 1..45)
-        require(!winningNumbers.contain(bonusNumber))
+        require(bonusNumber in 1..45) { "$PREFIX 보너스 번호 범위가 맞지 않습니다." }
+        require(!winningNumbers.contain(bonusNumber)) { "$PREFIX 보너스 번호가 중복됩니다." }
     }
 
     fun checkRating(lotto: Lotto): Rating {
