a1340bfcaf2f2f0644359d84490f092eeedf9863
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-13 16:43:04 +0900

    feat(Lotto): 예외처리 문구 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,10 +1,12 @@
 package lotto.model
 
+private const val PREFIX = "[ERROR]"
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
-        require(numbers.distinct().count() == numbers.size)
-        require(numbers.all { it in 1..45 })
+        require(numbers.size == 6) { "$PREFIX 갯수가 맞지 않습니다." }
+        require(numbers.distinct().count() == numbers.size) { "$PREFIX 중복이 있습니다." }
+        require(numbers.all { it in 1..45 }) { "$PREFIX 범위가 맞지 않습니다." }
     }
 
     fun getNumbers(): List<Int> = numbers
